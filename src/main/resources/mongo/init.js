9b32f29c0d1c76e7a6b9abcc922f8ae7dfbafa5c
Brehin Ethan <80754323+ebrehin@users.noreply.github.com>
2026-02-28 18:38:57 +0100

    Correction urls affiches + maj du port utilisé 8080 -> 8081

--- src/main/resources/mongo/init.js
+++ src/main/resources/mongo/init.js
@@ -6,11 +6,11 @@ db = db.getSiblingDB("posters_db");
 db.posters.drop();
 
 db.posters.insertMany([
-    { _id: "tt0111161", url: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NiYy._V1_.jpg",  titre: "The Shawshank Redemption" },
-    { _id: "tt0068646", url: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYx._V1_.jpg",  titre: "The Godfather" },
-    { _id: "tt0071562", url: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWRi._V1_.jpg",  titre: "The Godfather Part II" },
-    { _id: "tt0468569", url: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBn._V1_.jpg",  titre: "The Dark Knight" },
-    { _id: "tt0050083", url: "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0._V1_.jpg",  titre: "12 Angry Men" }
+    { _id: "tt0111161", url: "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg",  titre: "The Shawshank Redemption" },
+    { _id: "tt0068646", url: "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_SX300.jpg",  titre: "The Godfather" },
+    { _id: "tt0071562", url: "https://m.media-amazon.com/images/M/MV5BMDIxMzBlZDktZjMxNy00ZGI4LTgxNDEtYWRlNzRjMjJmOGQ1XkEyXkFqcGc@._V1_SX300.jpg",  titre: "The Godfather Part II" },
+    { _id: "tt0468569", url: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",  titre: "The Dark Knight" },
+    { _id: "tt0050083", url: "https://m.media-amazon.com/images/M/MV5BYjE4NzdmOTYtYjc5Yi00YzBiLWEzNDEtNTgxZGQ2MWVkN2NiXkEyXkFqcGc@._V1_SX300.jpg",  titre: "12 Angry Men" }
 ]);
 
 print("Collection posters initialisée avec " + db.posters.countDocuments() + " documents.");
